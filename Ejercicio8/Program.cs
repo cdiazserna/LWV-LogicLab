383fdd6cbebdfc2399f89788f3c404c2672bb4a6
Carlor Jr Diaz <cjunior90@gmail.com>
2023-09-04 21:23:45 -0500

    Ejercicio8 resuelto

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ejercicio8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Construir un programa que pida un número y luego diga si este número es par o no. 
+
+            //Datos de Entrada: int num
+            //Proceso % Módulo
+            //Datos de salida: Solo mensaje
+
+            //Declaración de variables:
+            int num;
+            bool esPar = false;
+
+            Console.Write("Ingrese número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0 ) //num = 5
+            {
+                esPar = true; 
+            }
+
+            if (esPar) // [if (!esPar)] -es igual a- [if (esPar == false)]
+            {
+                Console.WriteLine($"El número {num} es PAR");
+            }
+            else
+            {
+                Console.WriteLine($"El número {num} es IMPAR");
+            }
+        }
+    }
+}
