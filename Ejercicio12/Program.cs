2417028313c94e514d31366304303ecec8ee8920
Carlor Jr Diaz <cjunior90@gmail.com>
2023-09-15 20:57:20 -0500

    Ejercicio 12 resuelto

---
+++ Ejercicio12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace Ejercicio12
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programe un algoritmo que pida a usuario 2 números por pantalla y que muestre todos los números comprendidos entre ambos números incluyéndolos. 
+
+            //Declaración de variables
+            int num1, num2, min, max, numero;
+            const string MESSAGE = "Por favor ingresar un número entero válido";
+
+            Console.Write("Ingrese el primer número: ");
+            if (!int.TryParse(Console.ReadLine(), out num1)) //9
+            {
+                Console.WriteLine(MESSAGE);
+            }
+
+            Console.Write("Ingrese el segundo número: ");
+            if (!int.TryParse(Console.ReadLine(), out num2)) //2
+            {
+                Console.WriteLine(MESSAGE);
+            }
+
+            //Proceso
+            min = Math.Min(num1, num2);
+            max = Math.Max(num1, num2);
+
+            numero = min;
+            while (numero <= max)
+            {
+                Console.Write(String.Format("{0} ", numero));
+                numero++;
+            }
+        }
+    }
+}
