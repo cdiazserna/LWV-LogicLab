49a338421e17f36306468b38150deebdc50e539a
Carlor Jr Diaz <cjunior90@gmail.com>
2023-09-18 20:43:47 -0500

    Ejercicio 1 realizado correctamente

--- Ejercicio14/Program.cs
+++ Ejercicio14/Program.cs
@@ -2,11 +2,33 @@
 
 namespace Ejercicio14
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Elabora un algoritmo que imprima los números del 1 al 100. Que calcule la suma de todos los números pares por un lado, y por otro, la de todos los impares. 
+
+            //Declaración de variables
+            int sumaPares = 0, sumaImpares = 0;
+
+            for (int i = 1; i <= 100; i++) //i++ --> i = i + 1
+            {
+                Console.Write($"{i} ");
+
+                if (i % 2 == 0)
+                {
+                    sumaPares += i; // sumaPares = sumaPares + i
+                }
+                else
+                {
+                    sumaImpares += i; //sumaImpares = sumaImpares + i
+                }
+            }
+
+            Console.WriteLine("\n");
+            Console.Write(String.Format("La suma de los pares es = {0}", sumaPares));
+            Console.WriteLine("\n");
+            Console.Write(String.Format("La suma de los impares es = {0}", sumaImpares));
         }
     }
 }
