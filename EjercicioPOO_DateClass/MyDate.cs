b46551b0a0354b71803a98f396b681410835d418
Carlor Jr Diaz <cjunior90@gmail.com>
2023-10-05 17:25:53 -0500

    Mi primera app en POO

---
+++ EjercicioPOO_DateClass/MyDate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EjercicioPOO_DateClass
+{
+    public class MyDate
+    {
+        //Sección de prop
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        //Método ctor
+        public MyDate(int day, int month, int year) //day=5, month=8, year=2000
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        //Demás métodos
+        public string ShowDate()
+        {
+            return String.Format("{0}/{1}/{2}", Day, Month, Year); //  5/8/2000
+        }
+    }
+}
---
+++ EjercicioPOO_DateClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EjercicioPOO_DateClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de variables
+            int dia, mes, ano;
+
+            //Pedir datos por pantalla
+            Console.Write("Ingrese el día: ");
+            dia = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el mes: ");
+            mes = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el año: ");
+            ano = Convert.ToInt32(Console.ReadLine());
+
+            //Creación de objetos - Instancia de Clases
+            MyDate myDate = new MyDate(dia, mes, ano);
+
+            //Mostrar la fecha
+            Console.WriteLine(myDate.ShowDate());
+        }
+    }
+}
